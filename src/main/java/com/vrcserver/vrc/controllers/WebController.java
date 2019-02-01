dcb12a3672344dadae64341dfdd13246b4a98367
nguyenthanhduc <nguyenthanhduc@luvina.com>
2019-02-01 14:04:06 +0700

    ẤDASDSA

--- src/main/java/com/vrcserver/vrc/controllers/WebController.java
+++ src/main/java/com/vrcserver/vrc/controllers/WebController.java
@@ -3,6 +3,7 @@ package com.vrcserver.vrc.controllers;
 import com.vrcserver.vrc.dto.CarDTO;
 import com.vrcserver.vrc.dto.UserDTO;
 import com.vrcserver.vrc.services.AdminService;
+import com.vrcserver.vrc.services.VrcService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,11 @@ import javax.servlet.http.HttpSession;
 @RestController
 public class WebController {
     private AdminService adminService;
+    private VrcService vrcService;
 
-    public WebController(AdminService adminService) {
+    public WebController(AdminService adminService, VrcService vrcService) {
         this.adminService = adminService;
+        this.vrcService = vrcService;
     }
 
     @GetMapping(value = "/")
@@ -24,9 +27,39 @@ public class WebController {
         mav.setViewName("index");
         return mav;
     }
-    @GetMapping(value = "/them-xe")
-    public ModelAndView themXe(){
+    @GetMapping(value = "/web-login")
+    public ModelAndView login(HttpSession session) {
         ModelAndView mav = new ModelAndView();
+        if (session.getAttribute("user") != null) {
+            mav.setViewName("redirect:/them-xe");
+        } else {
+            mav.addObject("user", new UserDTO());
+            mav.setViewName("login");
+        }
+        return mav;
+    }
+    @PostMapping(value = "/web-loginProcess")
+    public ModelAndView getLogin(UserDTO userDTO, HttpSession session)  {
+        ModelAndView mav= new ModelAndView();
+        mav.addObject("user",userDTO = vrcService.login(userDTO));
+        if (userDTO.getId() != null) {
+            session.setAttribute("user", userDTO);
+            session.removeAttribute("error");
+            mav.setViewName("redirect:/them-xe");
+        } else {
+            session.setAttribute("error", userDTO.getMessage());
+            mav.addObject("user", new UserDTO());
+            mav.setViewName("login");
+        }
+        return mav;
+    }
+    @GetMapping(value = "/them-xe")
+    public ModelAndView themXe(HttpSession session){
+        ModelAndView mav = new ModelAndView();
+        if (session.getAttribute("user") == null){
+            mav.setViewName("redirect:/web-login");
+            return mav;
+        }
         mav.addObject("add",new CarDTO());
         mav.addObject("typeCar",adminService.getTypeCar());
         mav.addObject("listTypeCar",adminService.getTypeCar());
